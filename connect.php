8d84a95772221924d3455807d2cab780d7a796a4
Navjot <navjot.singh2472017@gmail.com>
2019-09-09 21:55:44 +0530

    First commit

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Waheguru
+ * Date: 9/4/2019
+ * Time: 10:06 AM
+ */
+echo "connect.php";
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Waheguru
+ * Date: 9/4/2019
+ * Time: 10:06 AM
+ */
+header("Location: connect.php");
+echo "Hello index.php";
